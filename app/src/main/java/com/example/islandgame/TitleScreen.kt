76ba7d8bb227fb0147ac5ad64b9f078c9478a026
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-11-17 19:59:00 -0800

    Added Title Screen activity

---
+++ app/src/main/java/com/example/islandgame/TitleScreen.kt
@@ -0,0 +1,11 @@
+package com.example.islandgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TitleScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_title_screen)
+    }
+}
